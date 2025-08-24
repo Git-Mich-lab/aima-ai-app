7edbee11afacd3c299d86b0f8b44e6293781069c
Git-Mich-lab <michaiahernest01@gmail.comm>
2025-08-24 20:55:53 +0100

    gemini model change

--- ai-chatapp/index.js
+++ ai-chatapp/index.js
@@ -24,7 +24,7 @@ if (!process.env.GEMINI_API_KEY) {
   process.exit(1);
 }
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
 
 // ----------------- Chat Route -----------------
 app.post('/chat', async (req, res) => {
